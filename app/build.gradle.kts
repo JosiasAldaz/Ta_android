b77bb7c93a8220ecf9950e539d0e24c688d81fa3
JosiasAldaz <josiasjoestar@hotmail.com>
2023-12-11 22:16:54 -0500

    Reproductor de video

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.github.bumptech.glide:glide:4.13.1")
-
+    implementation ("com.pierfrancescosoffritti.androidyoutubeplayer:core:11.0.1")
     
 
 
--- app/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/MainFragment.kt
@@ -7,10 +7,16 @@ import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.HeaderItem
 import androidx.leanback.widget.ListRow
 import androidx.leanback.widget.ListRowPresenter
+import androidx.leanback.widget.OnItemViewClickedListener
 import com.example.myapplication.R
+import androidx.leanback.widget.*
+import android.content.Intent
+import android.net.Uri
 
-data class Movie(val title:String,val year: Int,val poster: String)
-class MainFragment:BrowseSupportFragment() {
+
+data class Movie(val title:String,val year: Int,val poster: String,val urlyoutube: String)
+class MainFragment : BrowseSupportFragment() {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         title = getString(R.string.browse)
@@ -19,19 +25,27 @@ class MainFragment:BrowseSupportFragment() {
             val categoryTitle = "Category $categoryId"
 
             val listRowAdapter = ArrayObjectAdapter(CardPresenter())
-            listRowAdapter.addAll(0,(1..5).map {
+            listRowAdapter.addAll(0, (1..5).map {
                 Movie(
-                    "Title $it",
+                    "Guardians of the Galaxy $it",
                     2023,
-                    "https://loremflickr.com/176/313/dog?lock=$it"
+                    "https://th.bing.com/th/id/OIP.f7MWeWUGzZWdtS6EUANJKgHaHa?rs=1&pid=ImgDetMain/",
+                    "Wr5moiWHj9o"
                 )
-
             })
 
             val header = HeaderItem(categoryId.toLong(), categoryTitle)
             rowsAdapter.add(ListRow(header, listRowAdapter))
         }
         adapter = rowsAdapter
+
+        onItemViewClickedListener = OnItemViewClickedListener { itemViewHolder, item, rowViewHolder, row ->
+            if (item is Movie) {
+                val videoUrl = "https://www.youtube.com/watch?v=${item.urlyoutube}"
+                val youtubeIntent = Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl))
+                startActivity(youtubeIntent)
+            }
+        }
     }
 }
 
